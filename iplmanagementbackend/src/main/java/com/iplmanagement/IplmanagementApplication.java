3e3dbfa34fdacc77b731b74d56b6319acc36b08f
Prince <kevin.prince@capgemini.com>
2023-06-07 23:48:39 +0530

    initial commit

---
+++ iplmanagementbackend/src/main/java/com/iplmanagement/IplmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.iplmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplmanagementApplication.class, args);
+	}
+
+}
---
+++ iplmanagementbackend/src/test/java/com/iplmanagement/IplmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iplmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
