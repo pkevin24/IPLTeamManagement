09250c83d05ae87d3f5ee6181ca80f3467105adc
Jay Thummar <thummar024@gmail.com>
2023-06-13 12:55:18 +0530

    Cookie service implemented for auth gaurd.

--- frontend/src/app/auth.gaurd.ts
+++ frontend/src/app/auth.gaurd.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cookieService: CookieService) {}
 
   canActivate(): boolean {
-    const jwt = sessionStorage.getItem('jwtToken');
+    const jwt = this.cookieService.get('jwtToken');
     if (jwt) {
       return true; // Allow access to the home page if a valid JWT is present
     } else {
