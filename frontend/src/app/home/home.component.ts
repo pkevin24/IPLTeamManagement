766bb63115dcf95d0f89475fdbb4f648c8bcb37f
Jay Thummar <thummar024@gmail.com>
2023-06-14 10:40:51 +0530

    Created Home page cards and navbar.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor(private cookieService: CookieService) {}
 
+  logout() {
+    this.cookieService.delete('jwtToken');
+  }
 }
